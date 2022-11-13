79c94ffa869960663bdfc61229b3f0e93db701df
ricqCodes <princenwakanma1996@gmail.com>
2022-11-14 02:53:26 +0800

    fix: remove used variables

--- src/App.js
+++ src/App.js
@@ -111,13 +111,13 @@ const MainContainer = styled.div`
 
   @media (max-width: 24.5em) {
     ${AppContainer} & {
-      width: 55rem;
+      width: 53rem;
     }
   }
 
-  /* @media (max-width: 23em) {
+  @media (max-width: 23em) {
     ${AppContainer} & {
       width: 40rem;
     }
-  } */
+  }
 `;
