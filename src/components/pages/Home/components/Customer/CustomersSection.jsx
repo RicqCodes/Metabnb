2a397c8aa128254094744290e55c9128bbaf9442
ricqCodes <princenwakanma1996@gmail.com>
2022-11-14 22:05:45 +0800

    fix: remove used variables

--- src/components/pages/Home/components/Customer/CustomersSection.jsx
+++ src/components/pages/Home/components/Customer/CustomersSection.jsx
@@ -55,7 +55,7 @@ const CustomerContainer = styled.div`
     width: 15rem;
     padding: 0.5rem 0;
 
-    @media (max-width: 22em) {
+    @media (max-width: 23em) {
       width: 8.5rem;
     }
 
--- src/components/pages/Home/components/Discover/DiscoverSection.jsx
+++ src/components/pages/Home/components/Discover/DiscoverSection.jsx
@@ -48,7 +48,7 @@ const DiscoverContainer = styled.div`
     height: 40rem;
   }
 
-  @media (max-width: 22em) {
+  @media (max-width: 23em) {
     gap: 5rem;
     /* padding: 0 5rem; */
   }
