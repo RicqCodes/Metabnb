26084bd8b26a25fe34b8b62d457804c719c57b88
ricqCodes <princenwakanma1996@gmail.com>
2022-11-16 06:21:18 +0800

    fix: remove used variables

--- src/components/pages/Home/components/Discover/DiscoverSection.jsx
+++ src/components/pages/Home/components/Discover/DiscoverSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-import image from "./assets/image.svg";
+import image from "./assets/image.png";
 
 const DiscoverSection = () => {
   return (
